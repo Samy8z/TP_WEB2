59d55b1581b1a0ce6259fd652e83fe48d49b3d64
LejusVDP <julvandeputte@gmail.com>
2021-12-16 09:37:24 +0100

    modal & banner working

---
+++ scripts/tp-2-as.js
@@ -0,0 +1,29 @@
+
+let modal = document.getElementById('conf_modal')
+let cancel = document.getElementById('modal-cancel')
+let desac = document.getElementById('modal-desac')
+
+
+function closeModal() {
+    cancel.removeEventListener('click', closeModal)
+    desac.removeEventListener('click', closeModal)
+    modal.classList.add('hidden')
+}
+
+
+cancel.addEventListener('click', closeModal)
+desac.addEventListener('click', closeModal)
+
+
+
+let banner = document.getElementById('banner')
+let close = document.getElementById('banner-close')
+
+
+function closeBanner() {
+    close.removeEventListener('click', closeBanner)
+    banner.classList.add('hidden')
+}
+
+
+close.addEventListener('click', closeBanner)
