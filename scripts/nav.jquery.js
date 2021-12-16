f698481322f00762f3313ba9de4f3a8cff15add3
LejusVDP <julvandeputte@gmail.com>
2021-12-16 14:47:57 +0100

    14:46 nav bar marche bien mais j'aime pas

--- scripts/nav.jquery.js
+++ scripts/nav.jquery.js
@@ -8,3 +8,12 @@ $('#more').on('click', function click() {
     $('#navMore').fadeToggle(500)
     $('#navSolution').fadeOut(500)
 })
+
+$(document.body).click(function(e) {
+    if(!$(e.target).is($('#more'))&&!$.contains($('#more')[0],e.target) && !$(e.target).is($('#navMore'))&&!$.contains($('#navMore')[0],e.target) ) {
+        $('#navMore').fadeOut(500)
+    }
+    if(!$(e.target).is($('#solutions'))&&!$.contains($('#solutions')[0],e.target) && !$(e.target).is($('#navSolutions'))&&!$.contains($('#navSolutions')[0],e.target) ) {
+        $('#navSolutions').fadeOut(500)
+    }
+})
